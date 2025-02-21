bf77cd1cc418f9c1e9795f4398d42ca66e4f389e
0xNick404 <ngrimestt@gmail.com>
2025-02-20 22:11:22 -0400

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -2,24 +2,18 @@ import random
 
 # Function to generate a random key
 def getRandomNum():
-    return random.randint(32, 126)
+    return random.randint(0, 127)
 
 # Function to encrypt message
 def caesarCipher(text, shift):
     encryptedText = ""
     for c in text:
-        if 32 <= ord(c) <= 126:
-            encryptedText += chr(((ord(c) - 32 + shift) % 95) + 32)
-        else:
-            encryptedText += c
+        encryptedText += chr(((ord(c) - 32 + shift) % 128))
     return encryptedText
 
 # Function to decrypt message
 def caesarDecipher(message, shift):
     decryptedText = ""
     for c in message:
-        if 32 <= ord(c) <= 126:
-            decryptedText += chr(((ord(c) - 32 - shift + 95) % 95) + 32)
-        else:
-            decryptedText += c
+        decryptedText += chr(((ord(c) - shift) % 128))
     return decryptedText
